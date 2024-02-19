6cc3752d9f3c4c97682c84c2c4729feb0cf80a2f
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-02-19 18:28:45 -0300

    upload classes 19-02-24

---
+++ 1- hello-world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout << "Hello World" << endl; // [cout <<] is equivalent to [printf()] and [<< endl] is equivalent to [\n]
+	
+	// [<< endl] can only be used at the end of the line
+	
+	system("pause");
+	
+	return 0;
+}
---
+++ 2-variables.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int life = 0;				// integer type: 10, 25
+	char letter = 'K';			// char type: 'a', 'b'
+	double decimal1 = 5.2;		// double type: 5.47472743646, 3.34374436543
+	float decimal2 = 5.2;		// float type: 5.47, 3.3
+	bool alive= true;			// boolean type: true / false
+	string name = "Kevin";		// string type: "asasas", "vbvbvbvb"
+	
+	// using multiple lines to display:
+	
+	cout << life << "\n";
+	cout << letter << "\n";
+	cout << decimal1 << "\n";
+	cout << decimal2 << "\n";
+	cout << alive << "\n";
+	cout << name << "\n";
+	
+	cout << "Insert the number of lives: ";	// [cout << ] = [printf()]
+	cin >> life;
+	cout << "Insert a letter: ";
+	cin >> letter;
+	cout << "Insert a money value: ";
+	cin >> decimal1;
+	cout << "Insert a name: ";
+	cin >> name;
+	
+	// using only one line to display:
+	
+	cout << life << "\n" << letter << "\n" << decimal1 << "\n" << decimal2 << "\n" << alive << "\n" << name << "\n";
+	
+	system("pause");
+	
+	return 0;
+}
