dfb3068193ffd1a8b77b66aeff09fc1b3ef73249
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-02-22 16:25:25 -0300

    upload classes 22-02-24

---
+++ 5-Increments-operators-pre-increment-post-increment.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+		
+	// Increments and operators
+	
+	//	n1 = n1 + 1;		n1 += 1;	n1++;
+	//	n1 = n1 - 1;		n1 -= 1;	n1--;
+	//	n1 = n1 * 2;		n1 *= 2;
+	//	n1 = n1 / 2;		n1 /= 2;
+	
+	// Pre e post increment
+	
+	//	n1++		post increment
+	// ++n1			pre increment
+	//	n1--		post increment
+	// --n1			pre increment
+			
+	system("pause");
+	return 0;
+}
---
+++ 6-reversing-variables-values.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	//	-n			inverts the value but does not store that value in the variable
+	//	n =- n		inverts the value and store that value in the variable
+			
+	system("pause");
+	return 0;
+}
