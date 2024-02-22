e8383e4e7713811e8cf1178fbc44abe0c1ba0bc3
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-02-22 16:34:41 -0300

    upload classes 22-02-24

--- 5-Increments-operators-pre-increment-post-increment.cpp
+++ 5-Increments-operators-pre-increment-post-increment.cpp
@@ -3,21 +3,58 @@
 using namespace std;
 
 int main(){
-		
+
 	// Increments and operators
+
+	int n1 = 2;
+	n1 = n1 + 1;
+	cout << n1 << "\n\n";
+	n1 = 2;
+	n1 += 1;	
+	cout << n1 << "\n\n";
+	n1 = 2;
+	n1++;
+	cout << n1 << "\n\n";
 	
-	//	n1 = n1 + 1;		n1 += 1;	n1++;
-	//	n1 = n1 - 1;		n1 -= 1;	n1--;
-	//	n1 = n1 * 2;		n1 *= 2;
-	//	n1 = n1 / 2;		n1 /= 2;
+	n1 = 2;
+	n1 = n1 - 1;
+	cout << n1 << "\n\n";
+	n1 = 2;
+	n1 -= 1;
+	cout << n1 << "\n\n";
+	n1 = 2;
+	n1--;
+	cout << n1 << "\n\n";
+	
+	n1 = 2;
+	n1 = n1 * 2;
+	cout << n1 << "\n\n";
+	n1 = 2;
+	n1 *= 2;
+	cout << n1 << "\n\n";
+	
+	n1 = 2;
+	n1 = n1 / 2;
+	cout << n1 << "\n\n";
+	n1 = 2;
+	n1 /= 2;
+	cout << n1 << "\n\n";
 	
 	// Pre e post increment
 	
-	//	n1++		post increment
-	// ++n1			pre increment
-	//	n1--		post increment
-	// --n1			pre increment
-			
+	n1 = 2;
+	n1++;		//	post increment
+	cout << n1 << "\n\n";
+	n1 = 2;
+	++n1;		//	pre increment
+	cout << n1 << "\n\n";
+	n1 = 2;
+	n1--;		//	post increment
+	cout << n1 << "\n\n";
+	n1 = 2;
+	--n1;		//	pre increment
+	cout << n1 << "\n\n";
+	
 	system("pause");
 	return 0;
 }
--- 6-reversing-variables-values.cpp
+++ 6-reversing-variables-values.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 int main(){
+
+	int n = 2;	
+	cout << -n << "\n\n";			//	inverts the value but does not store that value in the variable
 	
-	//	-n			inverts the value but does not store that value in the variable
-	//	n =- n		inverts the value and store that value in the variable
-			
+	n=-n;
+	
+	cout << n << "\n\n";			//	inverts the value and store that value in the variable
 	system("pause");
 	return 0;
 }
