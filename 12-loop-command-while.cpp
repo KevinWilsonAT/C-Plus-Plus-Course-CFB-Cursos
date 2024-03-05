64460a10f24d4c0f9af146c608696b7b52d5ccde
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-03-05 16:04:57 -0300

    upload classes 05-03-24

---
+++ 12-loop-command-while.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+
+    /*
+        SYNTAX
+
+        increment type:
+
+        while(<counter><expression>){
+            <command_block>;
+            <counter>++
+        }
+
+        decrement type:
+
+        while(<counter><expression>){
+            <command_block>;
+            <counter>--
+        }
+
+        using keyboard input on <counter>
+
+        while(<counter><expression>){
+            <command_block>;
+            cin >> <counter>
+        }
+    */
+
+	system("pause");
+	return 0;
+}
