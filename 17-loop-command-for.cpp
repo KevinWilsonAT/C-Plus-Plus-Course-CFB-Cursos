08914cf7948a6e87e2ef9c30e747b559e668d7a3
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-03-26 17:19:31 -0300

    chore: Update classes from 23-02-24 to 21-03-24
    correction of the wrong filenames

---
+++ 17-loop-command-for.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+
+    /*
+        SYNTAX
+
+        increment type:
+
+        for(<counter>=<start_value>; <counter><=<end_value>; <counter>++){
+            <command_block>;
+        }
+
+        decrement type:
+
+        for(<counter>=<start_value>; <counter>>=<end_value>; <counter>--){
+            <command_block>;
+        }
+    */
+
+	system("pause");
+	return 0;
+}
