aa546484dcd2d845f532429e75196bffa488db36
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-04-16 20:26:36 -0300

    chore: upload class 16-04-24

---
+++ 22-Functions-pt1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	
+	    /*
+        
+		MUST BE DECLARED OUTSIDE THE MAIN SCOPE
+		
+		SYNTAX
+
+		No return and no parameters:
+		
+			void <function_name>(){
+				<command_block>
+			}
+		
+		No return and with parameters:
+		
+			void <function_name>(<parameter_type> <input_parameters>){
+				<command_block>
+			}
+		
+		With return and with parameters:
+		
+			<return_type> <function_name>(<parameter_type> <input_parameters>){
+				<command_block>
+				
+				return <output_parameter>
+			}
+			
+	*/
+	
+}
