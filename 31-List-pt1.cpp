eabcfaf3ee3434e3a16d65b27e3530eb429bdd42
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-06-17 17:25:21 -0300

    chore: upload class 11-06-24

---
+++ 31-List-pt1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(){
+
+    list<int> classes;
+    int sz;
+    list<int>::iterator it;
+
+    sz=10;
+
+    for(int i=0; i<sz; i++){
+        classes.push_front(i);
+    }
+
+    it=aula.begin();
+    advance(it,5);
+
+    cout << "List size: " << classes.size() << "\n\n";
+
+    /*
+    classes.sort();
+    classes.reverse();
+    */
+
+    sz = classes.size();
+    for(int i=0; i< sz; i++){
+        cout << classes.front() << "\n";
+        classes.pop_front();
+    }
+
+    return 0;
+}
