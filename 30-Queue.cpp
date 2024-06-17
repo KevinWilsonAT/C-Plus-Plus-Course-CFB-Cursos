44e54f4176e2f1eda0c39b125defecbf6370f008
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-06-17 16:50:31 -0300

    chore: upload class 05-06-24

---
+++ 30-Queue.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(){
+
+    queue <string> cards;
+
+    card.push("King of Hearts");
+    card.push("King of Spades");
+    card.push("King of Diamonds");
+    card.push("King of Trees");
+
+    cout << "Stack size: " << cards.size() << "\n";
+    cout << "First Card: " << cards.front() << "\n";
+    cout << "Last Card: " << cards.back() << "\n";
+
+    return 0;
+}
