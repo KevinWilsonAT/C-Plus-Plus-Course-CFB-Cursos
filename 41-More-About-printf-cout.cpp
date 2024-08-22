4137e67fee85929021aec4e1255e541e8fdec7e1
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-08-21 22:27:29 -0300

    chore: upload class 13-08-24

---
+++ 41-More-About-printf-cout.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+
+    //float pi = M_PI;
+    //printf("Pi Value: %f", pi);
+    //printf("Pi Value: %07.1f", pi);
+
+    //int num = 5;
+    //printf("Number Value: %d", num);
+    //printf("Number Value: %07d", num);
+
+    //float pi = M_PI;
+    //printf("Pi Value: %f", pi);
+
+    //int  num=15;
+
+    //cout << "Decimal Num Value: " << num << "\n";
+    //cout << "Hex Num Value: " << hex << num << "\n";
+    //cout << "Octal Num Value: " << oct << num << "\n";
+    //cout << "Hex Num Value: " << setbase(16) << num << "\n";
+
+    //float pi = M_PI;
+
+    //cout.precision(4);
+    //cout << "Pi Value:" << pi << "\n");
+
+    //cout.precision(-1);
+    //cout << "Pi Value:" << std::scientific << pi << "\n");
+
+    int num = 30;
+    cout << "Num Value:", << setw(10) << setfill('0') << num << "\n");
+
+    return 0;
+}
