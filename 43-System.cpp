cb720a7d90cada188679271be2c54df97a0a6581
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-09-28 21:09:51 -0300

    chore: upload class 28-09-24

---
+++ 43-System.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+
+    // system("color 10"); // changes the color palette of the console screen
+    system("dir"); // Display the contents of the this file folder
+    system("cls"); // Clear the console screen
+
+    return 0;
+}
